d5c85886dc387708b5da728f0d997de98d93151f
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2020-05-31 01:22:58 -0400

    project covid

--- js/animations.js
+++ js/animations.js
@@ -1,7 +1,11 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // Navbar drop down
-  $(".navbar-burger").click(function () {
+  $(".navbar-burger").click(function() {
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   });
+
+  if (isMobile.iOS()) {
+    $('a#covid').attr('href', 'https://testflight.apple.com/join/uNPRlb1W');
+  }
 });
